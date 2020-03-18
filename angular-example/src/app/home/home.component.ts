46787eb580adf0ef79db3bc41ad8e9027907c003
AoifeMcNeill <amcneill849@gmail.com>
2020-03-18 11:58:26 +0000

    finish Angular lab

--- angular-example/src/app/home/home.component.ts
+++ angular-example/src/app/home/home.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface TeamData{
+  teamName: string;
+  teamLocation: string;
+  teamNumber: number;
+}
+
+const TEAM_DATA: TeamData[] = [
+  { teamName: 'Simpsons', teamLocation: '742 Evergreen Terrace, Springfield', teamNumber: 5 },
+  { teamName: 'Ring Fellowship', teamLocation: 'Rivendell', teamNumber: 9 },
+  { teamName: 'FantasticFour', teamLocation: 'Baxter Building', teamNumber: 4 }
+]
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  teamTableColumns : string[] = ['teamName', 'teamLocation', 'teamNumber'];
+  dataSource = TEAM_DATA;
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
